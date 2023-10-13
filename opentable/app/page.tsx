a6e0fb1f90b71f3db055c7b05101101c72ad4b20
yogeshin <yogesh.c.shinde@capgemini.com>
2023-10-13 19:14:03 +0000

    Add files

--- opentable/app/page.tsx
+++ opentable/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import RestaurantCard from './components/RestaurantCard';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        <NavBar/>
+        <main>
+          <Header/>
+          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+            <RestaurantCard/>
+          </div>
+        </main>
+      </main>
     </main>
   )
 }
