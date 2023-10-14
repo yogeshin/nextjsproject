9bca392e2334a16f10e24a70d41ad05cb0a503bf
yogeshin <yogesh.c.shinde@capgemini.com>
2023-10-14 18:13:40 +0000

    add header

--- opentable/app/reserve/[slug]/page.tsx
+++ opentable/app/reserve/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import Header from "./Components/Header"
 import Form from "./Components/Form"
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Reservation | OpenTable',
+}
 
 export default function RestaurantReserve() {
   return (    
