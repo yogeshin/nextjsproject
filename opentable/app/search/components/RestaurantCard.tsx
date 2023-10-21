4b2eceb5178cc6472ac6d3ff0575671ca649c211
yogeshin <yogesh.c.shinde@capgemini.com>
2023-10-21 17:39:51 +0000

    Add files

--- opentable/app/search/components/RestaurantCard.tsx
+++ opentable/app/search/components/RestaurantCard.tsx
@@ -1,47 +1,49 @@
-import { PRICE } from "@prisma/client";
 import Link from "next/link";
+import { PRICE, Cuisine, Location } from '@prisma/client';
+import Price from "../../components/Price";
 
-interface Restaurant {    
-    id: number;
-    name: string;
-    main_image: string;
-    images: string[];
-    description: string;
-    open_time: string;
-    close_time: string;
-    slug: string;
-    price: PRICE;
-    location_id: number;
-    cuisine_id: number;
-    created_at: Date;
-    updated_at: Date;    
+
+type RestaurantCardType = {
+  id: number;
+  price: PRICE;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  slug: string;
 }
 
-export default function RestaurantCard() {
-    return (
-        <div className="border-b flex pb-5 ml-4">
-            <img
-                src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-                alt=""
-                className="w-44 rounded"
-            />
-            <div className="pl-5">
-                <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
-                <div className="flex items-start">
+export default function RestaurantCard({ restaurant }: {restaurant: RestaurantCardType}) {
+  return (
+    <div className="border-b flex pb-5 ml-5">
+      <img
+        src={restaurant.main_image}
+        alt=""
+        className="w-44 h-36 rounded"
+      />
+      <div className="pl-5">
+        <h2 className="text-3xl">{restaurant.name}</h2>
+        <div className="flex items-start">
                     <div className="flex mb-2">*****</div>
                     <p className="ml-2 text-sm">Awesome</p>
                 </div>
-                <div className="mb-9">
-                    <div className="font-light flex text-reg">
-                        <p className="mr-4">$$$</p>
-                        <p className="mr-4">Mexican</p>
-                        <p className="mr-4">Ottawa</p>
-                    </div>
-                </div>
-                <div className="text-red-600">
-                    <Link href="/restaurant/milestone-grill">View more information</Link>
-                </div>
-            </div>
+        <div className="mb-9">
+          <div className="font-light flex text-reg">
+            <Price price={restaurant.price}/>
+            <p className="mr-4 capitalize">{restaurant.cuisine.name}</p>
+            <p className="mr-4 capitalize">{restaurant.location.name}</p>
+          </div>
         </div>
-    )
+        <div className="text-red-600">
+          <Link href={`restaurant/${restaurant.slug}`}>View more information</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+
+
+
+
+
--- opentable/app/search/page.tsx
+++ opentable/app/search/page.tsx
@@ -1,57 +1,93 @@
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
-
-import { Metadata } from 'next'
+import { PRICE } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
- 
-export const metadata: Metadata = {
-  title: 'Search | OpenTable',
-}
+
+export const metadata = {
+  title: "Search | OpenTable",
+};
 
 const prisma = new PrismaClient();
 
-const fetchRestaurantByCity = async(city:string | undefined ) => {
-  if(!city) return await prisma.restaurant.findMany();
-  return await prisma.restaurant.findMany({
+interface SearchParams {
+  city?: string;
+  cuisine?: string;
+  price?: PRICE;
+}
+
+const select = {
+  id: true,
+  name: true,
+  main_image: true,
+  price: true,
+  cuisine: true,
+  location: true,
+  slug: true,
+  reviews: true,
+};
+
+const fetchLocations = async () => {
+  return prisma.location.findMany();
+};
+
+const fetchCuisines = async () => {
+  return prisma.cuisine.findMany();
+};
+
+const fetchRestaurantsBySearchParams = async ({
+  city,
+  cuisine,
+  price,
+}: SearchParams) => {
+  return prisma.restaurant.findMany({
     where: {
       location: {
         name: {
-          equals: city.toLowerCase()
-        }
-      }
+          equals: city?.toLowerCase(),
+        },
+      },
+      cuisine: {
+        name: {
+          equals: cuisine?.toLocaleLowerCase(),
+        },
+      },
+      price,
     },
-    select: {
-      id:true,
-      name:true,
-      price:true,
-      cuisine:true,
-      location:true,
-      main_image: true,
-      slug: true,
-    }  
+    select,
   });
-}
+};
 
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const restaurants = await fetchRestaurantsBySearchParams(searchParams);
+  const locations = await fetchLocations();
+  const cuisines = await fetchCuisines();
 
-export default async function Search({searchParams}:{ searchParams: { city: string }}) {
-  const restaurants = await fetchRestaurantByCity(searchParams.city);
-  console.log(restaurants);
   return (
-      <>        
-        <Header />
-        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          <SearchSideBar />
-          <div className="w-5/6">            
-            {restaurants.length ? (
-              <>
-                {restaurants.map( restaurant => {
-                  <RestaurantCard restaurant={restaurant}/>
-                })}
-              </>
-            ) : (<p>Sorry, we found no restaurant in {searchParams.city} area</p>)}            
-          </div>
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSideBar
+          locations={locations}
+          cuisines={cuisines}
+          searchParams={searchParams}
+        />
+        <div className="w-5/6">
+          {restaurants.length ? (
+            restaurants.map((restaurant) => {
+              return (
+                <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+              );
+            })
+          ) : (
+            <p>Sorry, we found no restaurants at this area</p>
+          )}
         </div>
-      </>
-  )
+      </div>
+    </>
+  );
 }
